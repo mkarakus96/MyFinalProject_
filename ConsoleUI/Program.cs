1306b81c780a961dc42da050bbb03ad7a300de7d
mkarakus96 <muhammedkarakus96@gmail.com>
2022-06-23 16:08:35 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        List<T> GetAllByCategory(int categoryId);
+    }
+}
